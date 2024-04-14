d3e6184bcdcd203e4c334bf3e24e1deb21196bd1
Menencia <lucien.stojcevski@gmail.com>
2024-04-14 12:38:14 +0200

    add primeng + add stat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    AppRoutingModule,
+    AutoCompleteModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AutoCompleteSelectEvent } from 'primeng/autocomplete';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  public selectedStat?: string;
+  public allSuggestions: string[];
+  public suggestions: string[];
+  public stats: string[] = [];
+
+  constructor() {
+    this.allSuggestions = ['atk', 'err'];
+    this.suggestions = [...this.allSuggestions];
+  }
+
+  public search(event: { query: string }): void {
+    this.suggestions = this.suggestions.filter((stat) =>
+      stat.includes(event.query),
+    );
+  }
+
+  public select(event: AutoCompleteSelectEvent): void {
+    this.stats.push(event.value);
+    this.suggestions = [...this.allSuggestions];
+    this.selectedStat = undefined;
+  }
 }
